858ddcbda5e8f4fc2ed3c1617a513983a8176a85
Yngrid <dhyks1310@gmail.com>
2023-10-24 15:12:06 -0300

    first commit

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent {
+
+  @Input()
+	gameType:string =""
+	@Input()
+	gamePrice:string = ""
+  constructor() { }
+
+}
